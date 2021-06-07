39ba16067fb4c629646d4e75f769f12fe731ab22
gabriel <gabrielfsm@al.insper.edu.br>
2021-06-07 16:24:53 -0300

    update5

--- tela_de_inicio.py
+++ tela_de_inicio.py
@@ -1,20 +1,20 @@
 import pygame
+from pygame import *
 from jogo_cobrinha import *
-
 
 
 #menu do jogo
 #variaveis da tela
 Largura = 1000
 altura = 500
-
+branco = (255, 255, 255)
 #relogio
 relogio = pygame.time.Clock()
 
 #tela do jogo
 tela = pygame.display.set_mode ((Largura, altura))
 
-centro = pygame.Vector2(Largura/2, altura/2) 
+centro = pygame.Vector2(200, altura/2) 
 
 game = True
 
@@ -30,8 +30,19 @@ while game:
             cobrinha1(tela, relogio)
         if distancia_ao_centro < 20 and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             cobrinha1(tela, relogio)
+   
     tela.fill((0, 0, 0))
+
+    
+  
+    txt='jogo da cobrinha'                                 
+    pygame.font.init()                                
+    fonte=pygame.font.get_default_font()              
+    fontesys=pygame.font.SysFont(fonte, 50)           
+    txttela = fontesys.render(txt, 1, (255,255,255))
+    tela.blit(txttela,(70, (altura/2)-80)) 
+
     pygame.draw.circle(tela, (0, 255, 0), centro, 20)
     relogio.tick(36)
     pygame.display.update()
-pygame.quit()
+pygame.quit
